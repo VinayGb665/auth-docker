1655072cd0da19f1085d27e0f3d082be3d013a28
Vinay G B <vgb1@rcinnet.rockwellcollins.com>
2019-01-28 16:06:18 +0530

    So many mods and learnings

--- index.js
+++ index.js
@@ -1,83 +1,116 @@
 var express =require('express')
 var app =express()
 var models = require('./models/models')
+const uuidv4 = require('uuid/v4')
 const bodyparser = require('body-parser')
 const bcrypt = require('bcrypt');
 const saltRounds = process.env.SALT_ROUNDS || 10;
-app.use(bodyparser.urlencoded({extended:true}))
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({ extended: true }))
 let userModel = models.userSchema
+
+
+
 app.listen(3000, (err) => {
-    console.assert(!err,'Error')
+    
+    console.assert(!err,'Error');
+
 })
-app.get('/v1/users/:username', (req,res) => {
-    var username = req.params.username || '';
-    userModel.find({name:username}, (err,results) => {
+
+
+
+app.get('/v1/users/:username?', (req,res) => {
+    console.log(req)
+    var username = req.params.username;
+    if(username){
+        userModel.find({username:username}, (err,results) => {
+                if(!err) res.send(results)
+                else res.send(err) 
+        })
+    }
+    else{
+        userModel.find({}, (err,results) => {
             if(!err) res.send(results)
             else res.send(err) 
-    })
-
-    /*
-    var x =process.env
-    var dat = new userModel({"name":"a","pass":"sss"});
-    dat.save((err) => {
-        if(!err) {
-            userModel.find({},(err,results) => {
-                if(!err) {
-                    res.send(results);
-                }
-                else{
-                    res.send("GFG")
-                }
-        
-        
-            });           
-        }
-        else res.send("GOne")
-    });
-    */
+        })
+    }
+    
 })
 
 
-app.post('/register', (req,res) => {
-    if(req.body.username && req.body.password ){
-        bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-            // Store hash in your password DB.
-            req.body.password=hash;
-            var newUser = new userModel(req.body);
-            newUser.save((err) => {
-                if(!err) res.send(newUser.password)
-                else res.send(err)
-            });
-          });
+app.post('/v1/users/:username', (req,res) => {
+    console.log(req)
+    var uname = req.body.username
+    if(uname && req.body.password ){
+    
+        userModel.find({username:uname}, (err,results) => {
+            if(err){
+                res.send({'status':'Error.Please Try again','err':err});
+            }
+            else{
+                if(results.length>0){
+                    res.send({'status':'Error.User Exists'})
+                }
+                else{
+                    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+                        // Store hash in your DB.
+                        req.body.password=hash;
+                        var newUser = new userModel(req.body);
+                        newUser.save((err) => {
+                            if(!err) res.send({'user':uname,'status':'Successfully registered'})
+                            else res.send({'status':'Error.Please Try again','err':err});
+                         });
+                      });
+                }
+            }
+
+        })
+
+
+       
+    }
+    else{
+
+        res.send({'status' : 'Check your parameters'})
     }
 
 
 });
 
-app.post('/login' , (req,res) => {
-    if(req.body.username && req.body.password ){
-        userModel.findOne({"name":req.body.username} , (err,results) => {
-            if(err) res.send("Nope")
+app.post('/v1/login' , (req,res) => {
+    console.log(req.body)
+    var uname = req.body.username
+    var pass = req.body.password;
+
+    if(uname && pass ){
+        userModel.findOne({"username":uname} , (err,results) => {
+            if(err || !results) { 
+                res.send(res.send({'status':'Login Failed - No such user'}))
+            }
             else {
-                bcrypt.compare(req.body.password,results.password, (err,res) =>{
-                    if(!err && res){
-                        res.send("OK")
+
+                bcrypt.compare(pass,results.password, (err,resu) =>{
+                    if(!err && resu){
+                        res.send({
+                            'user': uname,
+                            'token': uuidv4()
+                        })
                     }
                     else{
-                        res.send("Nope")
+                        res.send({'status':'Login Failed - Hash doesnt match'})
                     }
                 })
             }
         })
 
     }
+    else{
+        
+    }
+
 
 
 })
-
-
-
-
 
 
 
--- models/models.js
+++ models/models.js
@@ -1,8 +1,5 @@
 var mongoose =require('mongoose');
 var conn = mongoose.connect("mongodb://mongo:27017/try1",{ useNewUrlParser: true })
 console.assert(conn,"Error connecting to the DB");
-let userSchema = new mongoose.Schema({
-    name: 'string',
-    pass: 'string'
-},{strict:false})
+let userSchema = new mongoose.Schema({},{strict:false});
 module.exports.userSchema = mongoose.model("userModel",userSchema)
