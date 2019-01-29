00bda43712b348d80be98c24ecb2245f6a8fdf97
Vinay G B <vgb1@rcinnet.rockwellcollins.com>
2019-01-29 15:14:40 +0530

    Updates fixes and linting

--- models/models.js
+++ models/models.js
@@ -1,12 +1,40 @@
+/*
+
+*/
+
+// Packages,envs and dependencies -- >
+
 var mongoose =require('mongoose');
-var conn = mongoose.connect("mongodb://mongo:27017/try1",{ useNewUrlParser: true })
+const crypto = require('crypto');
+
+/*
+                                 ** TODO **
+        Make the DB more pluggable and configurable ## rn used as a service
+
+*/ 
+const mongo_port = process.env.MONGO_PORT || 27017; // Configurable mongodB port
+const coll_name = process.env.COL_NAME || 'users'; // Configurable collection name 
+
+// Connection -->
+
+var conn = mongoose.connect('mongodb://mongo:'+mongo_port+'/'+coll_name,{ useNewUrlParser: true })
 console.assert(conn,"Error connecting to the DB");
-let userSchema = new mongoose.Schema({},{strict:false});
-userSchema.pre('save', function(next) {                                                                                                                                        
+
+// Schemas -->
+
+let userSchema = new mongoose.Schema({},{strict:false}); 
+
+// Middleware for Schemas -->
+
+
+userSchema.pre('save', function(next) {     // Middleware to hash passwords after every save to DB                                                                                                                                    
+
     if(this.password) {                                                                                                                                                        
-        var salt = bcrypt.genSaltSync(10)                                                                                                                                     
-        this.password  = bcrypt.hashSync(this.password, salt)                                                                                                                
+                                                                                                                                       
+        this.password  = crypto.pbkdf2Sync(this.password, this.salt,1000, 64, `sha512`).toString(`hex`);                                                                                                                
     }                                                                                                                                                                          
     next()                                                                                                                                                                     
 }) 
+
+//exports
 module.exports.userSchema = mongoose.model("userModel",userSchema)
