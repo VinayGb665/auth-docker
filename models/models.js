6438eff27e8c7616293ce6cb02f0da47eac56248
Vinay G B <vgb1@rcinnet.rockwellcollins.com>
2019-01-29 15:12:22 +0530

    Middleware for hashing the passwords

--- models/models.js
+++ models/models.js
@@ -2,4 +2,11 @@ var mongoose =require('mongoose');
 var conn = mongoose.connect("mongodb://mongo:27017/try1",{ useNewUrlParser: true })
 console.assert(conn,"Error connecting to the DB");
 let userSchema = new mongoose.Schema({},{strict:false});
+userSchema.pre('save', function(next) {                                                                                                                                        
+    if(this.password) {                                                                                                                                                        
+        var salt = bcrypt.genSaltSync(10)                                                                                                                                     
+        this.password  = bcrypt.hashSync(this.password, salt)                                                                                                                
+    }                                                                                                                                                                          
+    next()                                                                                                                                                                     
+}) 
 module.exports.userSchema = mongoose.model("userModel",userSchema)
